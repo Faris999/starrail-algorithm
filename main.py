5949fea977e2236a9da0a4e7cb1d025893555825
Faris Hafizhan <farishafiz999@gmail.com>
2023-05-14 08:29:14 +0700

    fix hashing

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from mcts import mcts
 from copy import deepcopy
+import random
 
 class Entity():
-  def __init__(self, name, hp, atk, skill_dmg, type):
+  def __init__(self, name, hp, atk, type):
     self.name = name
     self.hp = hp
     self.atk = atk
-    self.skill_dmg = skill_dmg
+    # self.skill_dmg = skill_dmg
     self.type = type
 
   def __str__(self) -> str:
@@ -19,6 +20,7 @@ class State():
   def __init__(self, entities, turns):
     self.entities = entities
     self.turns = turns
+    self.num_turns = 0
     self.skill_points = 3
     
   def getPossibleActions(self):
@@ -34,6 +36,7 @@ class State():
     target = action.target
     new_state.entities[target].hp -= attacker.atk
     new_state.turns = new_state.turns[1:] + [new_state.turns[0]]
+    new_state.num_turns += 1
     
     if new_state.entities[target].hp <= 0:
       del new_state.entities[target]
@@ -47,10 +50,25 @@ class State():
     return all(players_hp_cond) or all(enemies_hp_cond)
 
   def getReward(self):
-    return 1
-  
+    enemies_hp_cond = [entity.hp <= 0 for key, entity in self.entities.items() if entity.type == 'enemy']
+    if not all(enemies_hp_cond):
+      return -9999999
+    
+    hp_left = sum([entity.hp for key, entity in self.entities.items() if entity.type == 'player'])
+    chars_alive = sum(1 for key, entity in self.entities.items() if entity.type == 'player')
+
+    return  -self.num_turns
+
   def __str__(self) -> str:
     return f'turn: {self.turns[0]}, {self.entities}'
+
+  def __repr__(self) -> str:
+    return str(self)
+
+  def __eq__(self, __value: object) -> bool:
+    if not isinstance(__value, State):
+      return False
+    return self.entities == __value.entities and self.turns == __value.turns
 
 class Action():
   def __init__(self, attacker, target):
@@ -64,19 +82,29 @@ class Action():
     return str(self)
 
   def __hash__(self) -> int:
-    return hash((self.attacker, self.target))
+    return hash(self.attacker.name + self.target)
+
+  def __eq__(self, __value: object) -> bool:
+    if not isinstance(__value, Action):
+      return False
+    return __value.attacker.name == self.attacker.name and self.target == __value.target
 
 state = State(entities={
-  'stelle': Entity('stelle', 100, 5, 'player'),
-  'march 7th': Entity('march 7th', 50, 20, 'player'),
-  'enemy1': Entity('enemy1', 100, 10, 'enemy'),
-  'enemy2': Entity('enemy2', 100, 10, 'enemy')
+  'stelle': Entity('stelle', 100, 40, 'player'),
+  'march 7th': Entity('march 7th', 50, 30, 'player'),
+  'enemy1': Entity('enemy1', 200, 5, 'enemy'),
+  'enemy2': Entity('enemy2', 50, 50, 'enemy')
 }, turns=['stelle', 'march 7th', 'enemy1', 'enemy2'])
 print(state)
 mcts = mcts(timeLimit=1000)
 
 while True:
-  bestAction = mcts.search(state)
+  if state.entities[state.turns[0]].type == 'enemy':
+    print('random')
+    bestAction = random.choice(state.getPossibleActions())
+  else:
+    print('searching')
+    bestAction = mcts.search(state)
   print(bestAction)
   state = state.takeAction(bestAction)
   print(state)
